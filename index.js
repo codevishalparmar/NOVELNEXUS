d104598e3ea4063439d95d59dbcb2c5be56015da
vishalparmar <vishalparmar633@gmail.com>
2025-06-01 00:38:58 +0530

    Updated backend index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 
 
 app.use(cors({
-    origin: 'http://localhost:5173',   // ✅ Your frontend URL
+    origin: 'https://novelnexus.vercel.app',   // ✅ Your frontend URL
     credentials: true,                  // ✅ Allow cookies to be sent
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"]
